7abb016eccc0291cf6fc4bd822bf22baed2f9155
theMorganObject <morgan.oshaughnessey@gmail.com>
2023-02-24 11:53:50 -1000

    finished optimizations

--- js/script.js
+++ js/script.js
@@ -43,19 +43,24 @@ allLinks.forEach(function (link) {
 
 /////////////////////////////////////////////////////////// Sticky Navigation
 
-const sectionHeroEl = document.querySelector("section-hero");
+const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     console.log(ent);
-    if (!ent.isIntersecting)
-      document.querySelector(".header").classList.add(".sticky");
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     // In the viewport
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
